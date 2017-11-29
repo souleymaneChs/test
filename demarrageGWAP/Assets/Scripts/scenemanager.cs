8dba346b4970e41cc431d1b7c738dd9ea8ae28bf
souleymaneChs <souleymaneabdellah@gmial.com>
2017-11-29 14:09:02 +0100

    Premier commit

---
+++ demarrageGWAP/Assets/Scripts/scenemanager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class scenemanager : MonoBehaviour {
+
+
+	public void ChangeScene (int i) {
+            Application.LoadLevel(i);
+            
+        }
+
+    }
+
